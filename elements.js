547339bfc7b09820d05b7abc938e76f1c1ea9d08
example-R <110983285+example-R@users.noreply.github.com>
2022-09-02 17:55:48 +0200

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,2 +1,2 @@
-var elements = ["prova"]
-var el = [{"name":"prova","content":"1"}]
+var elements = ["print","string"]
+var el = ["Print is the most basic function in python, it's used to output a string, a number or a variable.", "Strings are words, they can't run like a function, and they can't work like integrers (numbers)."]
